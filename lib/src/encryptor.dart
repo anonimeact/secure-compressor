9a8eca0099d10075b5dbbabd79f4439b015c99ba
Azri <muhammad.azri.f.s@gmail.com>
2024-05-24 13:07:52 +0700

    refactor(encryptor):refactor-methods-into-class-based-method-to-avoid-method-conflict

--- lib/src/encryptor.dart
+++ lib/src/encryptor.dart
@@ -1,54 +1,56 @@
 part of '../secure_compressor.dart';
 
-Future<String> encrypt(String data, String keyString, { String? ivString }) async {
-  // Initialize enctiprion using AES
-  final key = encriptor.Key.fromUtf8(keyString);
+class SecureCompressor {
+  static Future<String> encrypt(String data, String keyString,
+      {String? ivString}) async {
+    if (keyString.length != 32) {
+      throw ArgumentError('keyString must be 32 characters long.');
+    }
+    final key = encriptor.Key.fromUtf8(keyString);
+    final iv = encriptor.IV.fromUtf8(ivString ?? keyString.substring(0, 16));
+    final encrypter = encriptor.Encrypter(encriptor.AES(key));
+    final encryptedData = encrypter.encrypt(data, iv: iv);
+    return encryptedData.base64;
+  }
 
-  // The initali vector will using part off key if the iv is null
-  final iv = encriptor.IV.fromUtf8(ivString ?? keyString.substring(0, 16));
-  final encrypter = encriptor.Encrypter(encriptor.AES(key));
+  static String decrypt(String data, String keyString, {String? ivString}) {
+    if (keyString.length != 32) {
+      throw ArgumentError('keyString must be 32 characters long.');
+    }
+    final key = encriptor.Key.fromUtf8(keyString);
+    final iv = encriptor.IV.fromUtf8(ivString ?? keyString.substring(0, 16));
+    final encrypter = encriptor.Encrypter(encriptor.AES(key));
+    return encrypter.decrypt64(data, iv: iv);
+  }
 
-  // Encrypt the data
-  final encryptedData = encrypter.encrypt(data, iv: iv);
-  return encryptedData.base64;
-}
+  static String compress(String data) {
+    return String.fromCharCodes(gzip.encode(utf8.encode(data)));
+  }
 
-String decrypt(String data, String keyString, { String? ivString }) {
-  // Decode base64 dan initialize decription menggunakan AES
-  final key = encriptor.Key.fromUtf8(keyString);
+  static String uncompress(String data) {
+    return utf8.decode(gzip.decode(data.codeUnits));
+  }
 
-  // The initali vector will using part off key if the iv is null
-  final iv = encriptor.IV.fromUtf8(ivString ?? keyString.substring(0, 16));
-  final encrypter = encriptor.Encrypter(encriptor.AES(key));
+  static Future<String> compressAndEncrypt(String data, String keyString,
+      {String? ivString}) async {
+    final compressedData = compress(data);
+    final encryptedData =
+        await encrypt(compressedData, keyString, ivString: ivString);
+    return compress(encryptedData);
+  }
 
-  // Decrypt the data
-  return encrypter.decrypt64(data, iv: iv);
-}
+  static String uncompressAndDecrypt(String data, String keyString,
+      {String? ivString}) {
+    final uncompressedData = uncompress(data);
+    final decryptedData =
+        decrypt(uncompressedData, keyString, ivString: ivString);
+    return uncompress(decryptedData);
+  }
 
-// Compress string data using gzip
-String compress(String data) => String.fromCharCodes(gzip.encode(utf8.encode(data)));
-
-// Uncompress string data using gzip
-String uncompress(String data) => utf8.decode(gzip.decode(data.codeUnits));
-
-// keyString must be 32 char
-// ivString must be 16 char
-Future<String> compressAndEncrypt(String data, String keyString, { String? ivString }) async {
-  final encryptedData = await encrypt(compress(data), keyString, ivString: ivString);
-  return compress(encryptedData);
-}
-
-// keyString must be 32 char
-// ivString must be 16 char
-String uncompressAndDecrypt(String data, String keyString, { String? ivString }) {
-  final encryptedData = decrypt(uncompress(data), keyString, ivString: ivString);
-  return uncompress(encryptedData);
-}
-
-// if you want to generate data and save data in data/data/files
-// you can acces the data using, use Android Window Explorer
-Future<void> saveDataToLocal(String fileName, String data) async {
-  final directory = await getApplicationDocumentsDirectory();
-  final filePath = '${directory.path}/$fileName';
-  File(filePath).writeAsStringSync(data);
+  static Future<void> saveDataToLocal(String fileName, String data) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final filePath = '${directory.path}/$fileName';
+    final file = File(filePath);
+    await file.writeAsString(data);
+  }
 }
